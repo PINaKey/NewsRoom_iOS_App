f5d83628abdcf62c640b0ec04e942122b21ca9a6
Pinaki Bandyopadhyay <pinakibandyopadhyay@Pinakis-iMac.local>
2022-04-24 23:46:47 -0700

    initial commit

---
+++ News Room/NewsCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsCell.swift
+//  News Room
+//
+//  Created by Pinaki Bandyopadhyay on 04/02/1401 AP.
+//  Copyright © 1401 AP Pinaki Bandyopadhyay. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ News Room/NewsData.swift
@@ -0,0 +1,9 @@
+//
+//  NewsData.swift
+//  News Room
+//
+//  Created by Pinaki Bandyopadhyay on 03/02/1401 AP.
+//  Copyright © 1401 AP Pinaki Bandyopadhyay. All rights reserved.
+//
+
+import Foundation
